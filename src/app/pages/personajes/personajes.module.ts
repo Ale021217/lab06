9b2251278303c9e7149a222f5544ac5c1216247c
Ale021217 <alejandrojoserj@gmail.com>
2023-07-28 22:37:04 -0600

    Lab06

---
+++ src/app/pages/personajes/personajes.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+
+import { PersonajesRoutingModule } from './personajes-routing.module';
+import { PersonajesComponent } from './personajes.component';
+
+@NgModule({
+  declarations: [PersonajesComponent],
+  imports: [CommonModule, MatCardModule, PersonajesRoutingModule],
+})
+export class PersonajesModule {}
